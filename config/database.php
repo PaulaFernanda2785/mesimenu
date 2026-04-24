6e2de5caf40cf6338b1de3a23cac6ad16431453a
PaulaFernanda2785 <paulafernandacorrealima@gmail.com>
2026-04-24 20:55:51 -0300

    ajustes no utf 8 do banco de dados

--- config/database.php
+++ config/database.php
@@ -13,5 +13,6 @@ return [
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci',
     ],
 ];
